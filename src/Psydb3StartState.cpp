3b27354bf3f6f11a95445eeeeef2b80720c538b5
Dan <psydb3@nottingham.ac.uk>
2017-05-01 00:28:36 +0100

    start screen now has title and start message

--- src/Psydb3StartState.cpp
+++ src/Psydb3StartState.cpp
@@ -11,10 +11,12 @@ Psydb3StartState::~Psydb3StartState() {
 
 void Psydb3StartState::SetupBackgroundBuffer() {
 	m_pEngine->FillBackground(0xffffff);
+	m_pEngine->DrawBackgroundString(1200, 100, "Turrets", 0x332100, m_pEngine->GetFont("TOY SOLDIERS.ttf", 100));
+	m_pEngine->DrawBackgroundString(500, 500, "Press Space to Start", 0x000000, m_pEngine->GetFont("TOY SOLDIERS.ttf", 50));
 }
 
 void Psydb3StartState::Update() {
-
+	//m_pEngine->DrawScreenString(500, 500, "Press Space to Start", 0xffffff, m_pEngine->GetFont("TOY SOLDIERS.ttf", 50));
 }
 
 void Psydb3StartState::HandleKeys(int iKeyCode) {
